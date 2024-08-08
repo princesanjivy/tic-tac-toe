3f9d273a99259d1a5d0aafbd1698857a0ff1210d
princesanjivy <sanjivy.android@gmail.com>
2024-08-09 01:18:13 +0530

    added banner ad for web

--- lib/helper/show_banner_ad.dart
+++ lib/helper/show_banner_ad.dart
@@ -1,4 +1,7 @@
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html';
 import 'dart:math';
+import 'dart:ui' as ui;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -32,19 +35,39 @@ class BottomBannerAd {
     _bannerAd.load();
   }
 
+  Widget bannerForWeb() {
+    // ignore: undefined_prefixed_name
+    ui.platformViewRegistry.registerViewFactory(
+        "bannerAd",
+        (int viewID) => IFrameElement()
+          ..width = "468"
+          ..height = "60"
+          ..src = "adsterra_banner_ad.html"
+          ..style.border = "none");
+
+    return const SizedBox(
+      height: 60,
+      width: 468,
+      child: HtmlElementView(
+        viewType: "bannerAd",
+      ),
+    );
+  }
+
   Widget showBanner() {
     return kIsWeb
-        ? Container(
-            alignment: Alignment.center,
-            padding: EdgeInsets.all(2),
-            height: 26,
-            child: const Text(
-              "Copyright © princeappstudio.in",
-              style: TextStyle(
-                color: Colors.cyan,
-              ),
-            ),
-          )
+        // ? Container(
+        //     alignment: Alignment.center,
+        //     padding: EdgeInsets.all(2),
+        //     height: 26,
+        //     child: const Text(
+        //       "Copyright © princeappstudio.in",
+        //       style: TextStyle(
+        //         color: Colors.cyan,
+        //       ),
+        //     ),
+        //   )
+        ? bannerForWeb()
         : Container(
             alignment: Alignment.center,
             width: _bannerAd.size.width.toDouble(),
